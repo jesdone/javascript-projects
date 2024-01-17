afeaae17c07fa4d174b02682e90f33ffa8a1c835
jesdone <95374712+jesdone@users.noreply.github.com>
2024-01-16 19:09:53 -0500

    Progress on Part Two section two

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -1,38 +1,45 @@
 //Part Two Section One
 
-let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+// let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
 
-let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
-console.log(dna);
+// let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+// console.log(dna);
 
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
 
-let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
-let newString = dna.trim();
-console.log(newString);
+// let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+// let newString = dna.trim();
+// console.log(newString);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
 
-let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
-dna = dna.trim().toUpperCase();
-console.log(dna);
+// let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+// dna = dna.trim().toUpperCase();
+// console.log(dna);
 
 //3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
 
-let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
-console.log(dna);
+// let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
+// console.log(dna);
 
 //Part Two Section Two
 
 let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
 
 //1) Replace the gene "GCT" with "AGG", and then print the altered strand.
+
 dnaTwo.replace("GCT", "AGG");
+console.log(dnaTwo.replace("GCT", "AGG"));
 
 //2) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
+if (dnaTwo.indexOf("CAT")){
+    console.log("CAT gene found");
+}else{
+    console.log("CAT gene not found");
+}
 
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
 
