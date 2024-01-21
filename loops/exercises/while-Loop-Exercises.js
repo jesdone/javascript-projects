5b58fa7e9381f7eb5cabf9101eee17354d3be2f2
jesdone <95374712+jesdone@users.noreply.github.com>
2024-01-20 21:27:09 -0500

    While Loops Exercise Completed Code

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -3,9 +3,9 @@
 // another for the number of astronauts aboard, 
 // and the third for the altitude the shuttle reaches.
 
-let FuelLevel = 30000;
-let astronautsAboard = 6;
-let altitudeOfShuttle = 150000;
+let fuelLevel = 30000;
+let numAstronauts = 6;
+let altitude = 150000;
 
 
 /*Exercise #4: Construct while loops to do the following:
@@ -36,9 +36,18 @@ while (nums <= 0) {
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. 
 // Each iteration, decrease the fuel level by 100 units for each astronaut aboard. 
 // Also, increase the altitude by 50 kilometers.
+while (fuelLevel-100*numAstronauts >= 0) {
+  altitude += 50;
+  fuelLevel -= 100*numAstronauts;
+  }
 
 
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+let output = `The shuttle gained an altitude of ${altitude} km.`;
+
+if (altitude >= 2000) {
+output += " Orbit achieved!";
+}
