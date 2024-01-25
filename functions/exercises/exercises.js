02b32687636a91d7b70ccc9410177dd74f946cfb
jesdone <jesdone@gmail.com>
2024-01-24 22:46:09 -0500

    function exercise rectangle

--- functions/exercises/exercises.js
+++ functions/exercises/exercises.js
@@ -7,4 +7,16 @@ function makeLine(size){
     }
     return line;
   }
-console.log(line)
+  // console.log(makeLine(5));
+
+  function makeSquare(size){
+    let square = ''
+    for (let i = 0; i < size; i++) {
+      square += makeLine(size);
+      if (i < size - 1) {
+        square += '\n';
+      }
+    }
+    return square;
+  }
+  console.log(makeSquare(5))
